539d8dfe84a670c8c1aa5869138683b55e326e17
vgoylo <y_draiv.by@mail.ru>
2021-07-07 19:51:15 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# hash_key_reverse
---
+++ hash_key_reverse.rb
@@ -0,0 +1,13 @@
+
+def any_hash(hash)
+
+  hash.keys.reverse
+
+end
+
+puts any_hash ({
+  "The Batman" => "Bruce Wayne",
+  "Superman" => "Clark Kent",
+  "Wonder Woman" => "Diana Prince",
+  "Freakazoid" => "Dexter Douglas"
+})
